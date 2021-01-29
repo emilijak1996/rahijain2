8165b4fd462476b6aba8bdecf8b9c43a67cdc93b
emilijak1996 <emilijak19960501@gmail.com>
2021-01-29 14:17:08 +0000

    ddds

--- scrap2.py
+++ scrap2.py
@@ -13,7 +13,7 @@ OUTPUT_DIR	= 'output'
 results = []
 cities = []
 keywords = []
-TOT=450
+TOT=200
 tot=0
 
 file_location="input.xls"
@@ -51,7 +51,7 @@ proxy_index = 0
 try:
 	for keyword in keywords :
 		for city in cities :
-			time.sleep(10)
+			time.sleep(7)
 			if tot==TOT:
 				break
 			tot+=1
@@ -95,7 +95,7 @@ try:
 			try:
 				print(f"[city:{city}] [{keyword}] {total_page} pages")
 				for page in range(1,total_page +1) :
-					time.sleep(2)
+					time.sleep(1)
 					print(keyword," and ",city, " --",page," page loading...")
 					page_link=link + "/page-" +str(page)
 					success = False
